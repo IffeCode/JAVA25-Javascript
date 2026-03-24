b3f10aaff7fce4e940dfaa3eeb1e336f8f005d91
iffe <iffe.h@hotmail.com>
2026-03-24 11:19:54 +0100

    mer text i js!

--- Javascript-lektion1/script.js
+++ Javascript-lektion1/script.js
@@ -8,3 +8,21 @@ let age = 100;
 age = "Testar bara";
 
 console.log(age);
+
+let isRaining = true;
+console.log(isRaining);
+
+console.log(typeof age);
+
+const num = 3;
+console.log(typeof num);
+
+const arr = [3, 657, 100, 2];
+console.log(arr);
+
+console.log(arr.length);
+
+arr[0]= "En string"; //Får byta ut innehållet i ett element
+console.log(arr);
+
+//arr= 100; // Får inte byta ut innehållet i en const variabel
