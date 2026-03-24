df6e2d690d0c3387e2c71dbeebf8adba7eb0efc3
iffe <iffe.h@hotmail.com>
2026-03-24 11:45:41 +0100

    if sats i js!

--- Javascript-lektion1/script.js
+++ Javascript-lektion1/script.js
@@ -34,8 +34,37 @@ console.log("3" === 3);
 let val;
 
 if(val){
-    console.log("val innehåller bågot")
+    console.log("val innehåller bågot") //alt + shift + pil ner för att kopiera något
 }
 else{
     console.log("val är undefined")
 }
+
+
+for(let i=0; i<10; i++){
+    console.log(i);
+}
+
+const trees = ['Oak', 'Bristlecone', 'Birch', 'Redwood'];
+
+for(let i=0; i<trees.length; i++){
+    console.log(trees[i])
+}
+
+for(const tree of trees){
+    console.log(tree)
+}
+
+let randomNumber = Math.round( Math.random()*10 );
+
+// Används i situationer när man inte vet på förhand hur många gånger man be
+while(randomNumber != 3){
+    console.log(randomNumber);
+    randomNumber = Math.round( Math.random()*10 );
+}
+
+
+console.log('Three!');
+
+// i++;
+// i = i+1;
