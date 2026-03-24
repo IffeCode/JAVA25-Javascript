00a9a0a09c5adcadcbefebc6330c9ccd2fc6674c
iffe <iffe.h@hotmail.com>
2026-03-24 11:24:31 +0100

    if sats i js!

--- Javascript-lektion1/script.js
+++ Javascript-lektion1/script.js
@@ -26,3 +26,16 @@ arr[0]= "En string"; //Får byta ut innehållet i ett element
 console.log(arr);
 
 //arr= 100; // Får inte byta ut innehållet i en const variabel
+
+
+console.log("3" == 3);
+console.log("3" === 3);
+
+let val;
+
+if(val){
+    console.log("val innehåller bågot")
+}
+else{
+    console.log("val är undefined")
+}
